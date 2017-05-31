8271d3eadfb72bdd22e6a188f74cdf60c4a99008
Diemano Nóbrega <brunodiemano@gmail.com>
2017-05-31 12:07:20 -0300

    Changes in the code and images

---
+++ frontend/local-weather/js/script.js
@@ -0,0 +1,105 @@
+$(document).ready(function () {
+
+    if(navigator.geolocation){
+        var now = new Date();
+        var h = now.getHours();
+        var m = now.getMinutes();
+        navigator.geolocation.getCurrentPosition(function(position){
+            var lati = position.coords.latitude;
+            var longi = position.coords.longitude;
+            var api = "http://api.openweathermap.org/data/2.5/weather?lat="+lati+"&lon="+longi+"&appid=1039cb33d63eb371e635c7cd8c14cf9f";
+            $.getJSON(api, function(data){
+            $('.temper').html(data.main.temp+" F");
+            switch (data.weather[0].description){
+                case "clear sky":
+                    if(h>4&&h<18){
+                    $('body').css({"background-image":"url('assets/clear-sky-day.jpg')"});
+                    $('.tempo').html("<i class='wi wi-day-sunny'></i>");
+                    }else{
+                    $('body').css({"background-image":"url('assets/clear-sky-night.jpg')"});
+                    $('.tempo').html("<i class='wi wi-night-clear'></i>");
+                    }
+                    break;
+                case "few clouds":
+                    if(h>4&&h<18){
+                        $('body').css({"background-image":"url('assets/few-clouds-day.jpg')"});
+                        $('.tempo').html("<i class='wi wi-day-cloudy'></i>");
+                    }else{
+                        $('body').css({"background-image":"url('assets/few-clouds-night.jpg')"});
+                        $('.tempo').html("<i class='wi wi-night-alt-cloudy'></i>");
+                    }
+                    break;
+                case "scattered clouds":
+                    if(h>4&&h<18){
+                        $('body').css({"background-image":"url('assets/scattered-clouds-day.jpg')"});
+                        $('.tempo').html("<i class='wi wi-cloud'></i>");
+                    }else{
+                        $('body').css({"background-image":"url('assets/scattered-clouds-night.jpg')"});
+                        $('.tempo').html("<i class='wi wi-cloud'></i>");
+                    }
+                    break;
+                case "broken clouds":
+                    if(h>4&&h<18){
+                        $('body').css({"background-image":"url('assets/broken-clouds-day.jpg')"});
+                        $('.tempo').html("<i class='wi wi-cloudy'></i>");
+                    }else{
+                        $('body').css({"background-image":"url('assets/broken-clouds-night.jpg')"});
+                        $('.tempo').html("<i class='wi wi-cloudy'></i>");
+                    }
+                    break;
+                case "shower rain":
+                    if(h>4&&h<18){
+                        $('body').css({"background-image":"url('assets/shower-rain.jpg')"});
+                        $('.tempo').html("<i class='wi wi-rain'></i>");
+                    }else{
+                        $('body').css({"background-image":"url('assets/shower-rain.jpg')"});
+                        $('.tempo').html("<i class='wi wi-rain'></i>");
+                    }
+                    break;
+                case "rain":
+                    if(h>4&&h<18){
+                        $('body').css({"background-image":"url('assets/rain.jpg')"});
+                        $('.tempo').html("<i class='wi wi-day-hail'></i>");
+                    }else{
+                        $('body').css({"background-image":"url('assets/rain.jpg')"});
+                        $('.tempo').html("<i class='wi wi-night-alt-rain'></i>");
+                    }
+                    break;
+                case "thunderstorm":
+                    if(h>4&&h<18){
+                        $('body').css({"background-image":"url('assets/thunderstorm-day.jpg')"});
+                        $('.tempo').html("<i class='wi wi-day-snow-thunderstorm'></i>");
+                    }else{
+                        $('body').css({"background-image":"url('assets/thunderstorm-night.jpg')"});
+                        $('.tempo').html("<i class='wi wi-night-alt-snow-thunderstorm'></i>");
+                    }
+                    break;
+                case "snow":
+                    if(h>4&&h<18){
+                        $('body').css({"background-image":"url('assets/snow.jpg')"});
+                        $('.tempo').html("<i class='wi wi-day-snow'></i>");
+                    }else{
+                        $('body').css({"background-image":"url('assets/snow-night.jpg')"});
+                        $('.tempo').html("<i class='wi wi-night-alt-snow'></i>");
+                    }
+                    break;
+                case "mist":
+                    if(h>4&&h<18){
+                        $('body').css({"background-image":"url('assets/mist.jpg')"});
+                        $('.tempo').html("<i class='wi wi-day-fog'></i>");
+                    }else{
+                        $('body').css({"background-image":"url('assets/mist.jpg')"});
+                        $('.tempo').html("<i class='wi wi-night-fog'></i>");
+                    }
+                    break;
+
+            }
+            $('.textCity').html(data.name);
+            $('.horario').html(("0"+h).slice(-2)+":"+("0"+m).slice(-2));
+        })
+
+
+
+    })
+    }
+})
